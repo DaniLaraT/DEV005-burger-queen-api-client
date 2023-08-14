fb844aad7b76317b0b1787f0eaf85c1569883d14
yanet <yanettr2024@gmail.com>
2023-08-14 10:12:52 -0500

    creación,modificación e eliminación  de carpetas, componentes

--- src/views/waiter/menuBreakfast.jsx
+++ src/views/waiter/menuBreakfast.jsx
@@ -1,7 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './menuBreakfast.css'
+
 const MenuBreakfast = () => (
     <>
+      <div className='body'>
       <h1>PAGINA DE DESAYUNO</h1>
       <span>aqui podras hacer la orden de desayuno</span>
+      </div>
     </>
   )
   
