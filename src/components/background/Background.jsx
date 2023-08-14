fd6a47fc2ba730ddaaba568cc8252703ddc12055
yanet <yanettr2024@gmail.com>
2023-08-13 22:11:04 -0500

    arreglos en página404

---
+++ src/components/background/Background.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import react from 'react';
+import './background.css'
+
+const Background = () => {
+
+  return (
+
+    <figure className='content-background'>
+        <img src="/img/background.png" alt="background" />
+    </figure>
+  );
+}
+
+export default Background;
--- src/views/page404/page404.jsx
+++ src/views/page404/page404.jsx
@@ -1,8 +1,15 @@
-const Page404 = () => (
-  <>
-    <h1>Página no encontrada</h1>
-    <span>La página solicitada, no existe</span>
-  </>
-)
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Page404.css';
+import background from '/img/background.png';
 
-export default Page404
+const Page404 = () => (
+  <div className="page-content">
+    <h1 className="h1">Oops, ¡Esta página no existe!</h1>
+    <img src={background} alt="Burger Error" className="burger-image" />
+    <p className="error-message">Parece que has llegado a un territorio desconocido. La hamburguesa se comió esta página.</p>
+    <a href="/" className="back-link">Volver al inicio</a>
+  </div>
+);
+
+export default Page404;
