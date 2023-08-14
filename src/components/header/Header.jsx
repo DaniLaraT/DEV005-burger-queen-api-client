ff7ddfe6dd4457d5ee9876c238b0bcd670fe3d8d
yanet <yanettr2024@gmail.com>
2023-08-14 00:03:10 -0500

    agregar componentes:Title,Navbar,Header con sus respectivos css

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import PropTypes from 'prop-types';
+import NavBar from './NavBar';
+import Logo from './Logo'; 
+import Title from './Title'; 
+
+function Header({ title }) {
+  return (
+    <header className="header">
+      <NavBar />
+      <Logo /> 
+      <Title title={title} />
+    </header>
+  );
+}
+
+// Define las expectativas de la prop 'title'
+Header.propTypes = {
+  title: PropTypes.string.isRequired, // Espera una prop de tipo string y es requerida
+};
+
+export default Header;
+
